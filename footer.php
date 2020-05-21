dcf689e6c2b38e37e465171268abb0f1e31577ec
HayleyCollopy <61652264+HayleyCollopy@users.noreply.github.com>
2020-05-21 15:52:25 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,29 @@
+<!--      FOOTER STARTS-->
+      <footer class="container-fluid">
+          <div class="footer-content">
+              <div class="text-center">
+<!--              SOCIAL MEDIA ICONS GO HERE-->
+              <a href="https://facebook.com/GumalaTrust"><img class="icons" src="images/facebook%20icon.png"></a>
+              <a href="https://au.linkedin.com/company/gumala-trust"><img class="icons" src="images/linkedin%20icon.png"></a>
+              <a href="https://instagram.com"><img class="icons" src="images/instagram%20icon.png"></a>
+              </div>
+              
+              
+              <div class="row">
+                  
+              <div class="footerbox col-xs-6 text-center">
+              <p>CONTACT US</p>
+                  </div>
+                <div class="footerbox col-xs-6 text-center">
+              <p>TERMS &amp; CONDITIONS</p>
+                  </div>
+              </div>
+              </div>
+          
+          
+      </footer>
+<!--      FOOTER ENDS-->
+      
+  </body>
+</html>
+<?php wp_footer(); ?>
