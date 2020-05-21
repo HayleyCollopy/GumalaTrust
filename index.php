4698b55fb9246a214f842a3fff941b03a8e96299
HayleyCollopy <61652264+HayleyCollopy@users.noreply.github.com>
2020-05-21 15:52:36 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+      
+     <!--       WELCOME CONTAINER START-->
+      <section class="container-fluid aboutbg text-center notfooter">
+          
+          <div class="container ">
+                
+              <div class="welc"><h2>WELCOME</h2></div>
+               
+            
+                
+              
+        
+          
+              <p class="welcome-text">The Gumala Trust has a vital role as the Trustee of the General Gumala Foundation, and as such, has legal responsibilities to appropriately manage and supervise the monies received and distributed on behalf of the Foundation under the terms of its Trust Deed.</p>
+
+                  <p class="welcome-text">Since 1997, The Gumala Trust has been acting as a ‘safe pair of hands’ to ensure the best interest of our Beneficiaries, the Traditional Owners of the Yinhawangka, Bunjima and Nyiyaparli people, are being met both now and in the future...</p>
+      
+<!--              BUTTON-->
+              <button type="button" class="btn btn-primary">BECOME A BENEFICIARY</button>
+        </div>
+                    
+      </section>
+      <!--       WELCOME CONTAINER END-->
+      
+      <section class="container-fluid meetcarolbg notfooter">
+          <div class="container">
+              <div class="row">
+                  <div class="trustlogo"><img src="images/gumala-logo-icon.png"></div>
+                  
+                  <div class="col-md-6 meet-responsive">
+                      <h4 class="carol">MEET CAROL</h4>
+                      <p class="date">February 28, 2019</p>
+                      <p class="carolp">In January, the Gumala Trust (GIPL) congradulated Gumala Beneficiary Carol Lee Tucker when she took ownership of her home in Tom Price. Carol took advantage of the opportunity offered by GIPL last year for beneficiaries to purchace Gulama investment houses. The GIPL board is keen to see more Beneficiaries obtain their homes.
+                      </p>
+                      <p class="seemore">See more...</p>
+          
+          
+                  </div>
+                  
+                  <div class="col-md-6 meet-responsive">
+                      <img class="img-responsive" alt="carol" src="images/carol-img.jpg">
+                  </div>
+                  
+              </div>
+          </div>
+      </section>
+      
+      <section class="container-fluid meetdavidbg notfooter">
+          <div class="container">
+              <div class="row">
+                  
+                  <div class="col-md-6 meet-responsive">
+                  <img class="img-responsive" alt="david" src="http://206.189.45.97/~mesh4/wp-content/themes/GumalaTrust/images/david-img.jpg">
+                  </div>
+                  
+                  <div class="col-md-6 meet-responsive">
+                      <h4 class="david">MEET DAVID</h4>
+                      <p class="date">March 15, 2019</p>
+                      <p class="davidp">The Gumala Trust (GIPL) would like to congratulate Gumala Beneficiary David Moses Martin who took ownership of his home in South Hedland this week. David took advantage of the opportunity offered by GIPL last year for Beneficiaries to purchase Gumala investment houses. The GIPL Board is keen to see more Beneficiaries obtain their own homes and will consider sales of its residential properties in Tom Price and South Hedland to Beneficiaries. David is pictured here with Gumala Trust Executive Officer John Raftis.
+                      </p>
+                      <p class="seemore">See more...</p>
+          
+          
+                  </div>      
+              </div>
+          </div>
+      </section>
+
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
