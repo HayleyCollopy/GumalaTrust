250be2dbe3dab41567d980e71940169cecdbdcad
georgiaearnshaw <61772678+georgiaearnshaw@users.noreply.github.com>
2020-05-26 20:40:13 +0800

    Update beneficiaries.php

--- beneficiaries.php
+++ beneficiaries.php
@@ -11,41 +11,20 @@ Template Name: BENEFICIARIES Page
       <section class="container-fluid aboutbg text-center notfooter">
           
           <div class="container">
-                
-              <div class="beneficiary"><h2>BENEFICIARIES</h2></div>
               
-              <div class="sub-title-beneficiary"><h2>What is a Beneficiary?</h2></div>
-               
-            
-                
+                <h2><?php the_field('titleforbeneficiaries'); ?></h2><!--  custom field for the aboutme content-->
+    
+               <p class="sub-title-beneficiary"><?php the_field('subtitleforbeneficiary'); ?></p>
               
-        
-          
-              <p class="beneficiary-text">A Beneficiary is a person entitled to receive benefits from the General Gumala Foundation (GGF). To be a Beneficiary of this Foundation, you must be recognised as an individual of one of the three language groups: Bunjima, Nyiyaparli or Yinhawangka.
-                  
-                  <br><br>
-                  
-                  Gumala has approximately 1,600 registered adult Beneficiaries. In making a decision about who qualifies as a Beneficiary, the Trustee is bound by the Trust Deed to consider the following:</p>
-                      
-                       <ul class="beneficiary-text">
-<li>Aboriginal Tradition.</li>
-<li>The findings of any 'bona fide' genealogical studies, landownership studies, or land boundary studies that may at any time, or from time to time, be carried out.</li>
-<li>Whether a person regards him/herself as a Traditional Owner and is accepted by other Traditional Owners as a Traditional Owner.</li>
-<li>Any relevant determination of a court or tribunal.</li>
-<li>The relationship of a person to the area of the Traditional Lands in terms of family, clan, kinship, traditional occupation rights by marriage or affiliation, usufructuary rights and all other connections to the land recognised by Aboriginal Tradition.</li>
-<li>The views of any Traditional Owner Council.</li></ul>
- 
+              <p class="beneficiary-text"><?php the_field('beneficiarytext'); ?></p>
+
+            <ul class="beneficiary-text"><?php the_field('beneficiarytexttwo'); ?></ul> 
               
-              <p class="beneficiary-text">
-If you become a Beneficiary, you will be entitled to receive benefits from the GGF. As a Member you will be able to access these entitlements through programs, which have been designed to assist in the alleviation of poverty, sickness, suffering, distress, misfortune or destitution.
-                  
-                  <br><br>
-                  
-If you have recently turned 18, you are encouraged to put in a joint application for approval.</p>
-              
+              <p class="beneficiary-text"><?php the_field('beneficiarytextthree'); ?></p>
+       
               
 <!--              BUTTON-->
-              <a href="/imagestwo/joint-application-for-beneficiary-status_membership.pdf" download="Gumala Beneficiary Application"> <button type="button" class="btn btn-primary">BECOME A BENEFICIARY</button></a>
+              <a href="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/joint-application-for-beneficiary-status_membership.pdf" download="Gumala Beneficiary Application"> <button type="button" class="btn btn-primary">BECOME A BENEFICIARY</button></a>
         </div>
                     
       </section>
@@ -58,19 +37,19 @@ If you have recently turned 18, you are encouraged to put in a joint application
               <div class="row">
                   <div class="trustlogo"><img src="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/gumala-logo-icon.png"></div>
                   
-                  <div class="col-md-6 meet-responsive">
-                 <h4 class="application-process">Our Application Reviewing Process</h4>
-                      <p class="members-info">Once an application is sent to us, the application is sent to the Foundation Application Review Committee for assessment and review. This committee make recommendations to the GAC and GIPL Boards respectively about the applications.
-                          
-                          <br><br>
-
-The GIPL Board approves Beneficiary status. The GAC Board approve Memberships. For this reason, the committee is made up of TO Directors from both the GIPL and GAC Boards. This ensures the decision making is consistent regarding the outcomes of applications.
-                      </p>
-          
+                  
+                <div class="col-md-6 meet-responsive">
+                    
+                <p class="application-process"><?php the_field('applicationprocess'); ?></p>
+                    
+                     <p class="members-info"><?php the_field('membersinfo'); ?></p>
+                    
             </div>
                   
                   <div class="col-md-6 meet-responsive">
-                      <img class="img-responsive" alt="carol" src="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/application-image-three.png">
+                      
+                       <img class="img-responsive" alt=new-member-image” src="<?php the_field('applicationimage'); ?>">
+                      
                   </div>
                   
               </div>
@@ -81,16 +60,20 @@ The GIPL Board approves Beneficiary status. The GAC Board approve Memberships. F
           <div class="container">
               <div class="row">
                   
+                  
                   <div class="col-md-6 meet-responsive">
-                  <img class="img-responsive" alt="david" src="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/members-image.png">
+                        <img class="img-responsive" alt=“application-image” src="<?php the_field('membersimage'); ?>">
+
+               
                   </div>
                   
                   <div class="col-md-6 meet-responsive">
-                      <h4 class="application-reviewing">Updating Information</h4>
-                      <p class="beneficiary-text">If you are already a Beneficiary or a Member and would like to update your personal details, we encourage you to fill out a Personal Information Form.
-                      </p>
                       
-                       <a href="/imagestwo/personal-information-form.pdf" download="Gumala Beneficiary Application"> <button type="button" class="btn btn-update-info">UPDATE INFORMATION</button></a>
+                      <p class="application-reviewing"><?php the_field('updatinginformation'); ?></p>
+                      
+                       <p class="beneficiary-text"><?php the_field('membersinformation'); ?></p>
+                      
+                       <a href="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/personal-information-form.pdf" download="Gumala Beneficiary Application"> <button type="button" class="btn btn-update-info">UPDATE INFORMATION</button></a>
           
           
                   </div>      
