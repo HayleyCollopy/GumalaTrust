750fe5c034e9f6022a9829799d25639424c6a417
HayleyCollopy <61652264+HayleyCollopy@users.noreply.github.com>
2020-06-01 14:16:05 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -30,3 +30,18 @@ add_filter('nav_menu_css_class', 'add_menu_list_item_class', 1, 3);
 	}
 
 ?>
+<?php
+function sidebar() {
+
+	register_sidebar( array(
+		'name'          => 'sidebar',
+		'id'            => 'sidebar1',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<p class="rounded">',
+		'after_title'   => '</p>',
+	) );
+
+}
+add_action( 'widgets_init', 'sidebar' );
+?>
