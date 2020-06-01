e37280fd242f2b4a754dcd16bcf04d755bd16bf2
HayleyCollopy <61652264+HayleyCollopy@users.noreply.github.com>
2020-06-02 03:34:02 +0800

    Create board-of-directors.php

---
+++ board-of-directors.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Template Name: Board of Directors
+*/
+?>
+
+<?php include (TEMPLATEPATH . '/new_small_header.php');  /* Tells WordPress to include the smaller header header.php */ ?> 
+
+ <section class="container-fluid">
+          <div class="container">
+              <div class="row">
+                  
+                  <div class="col-md-6 meet-responsive">
+                      <h4>MEET CAROL</h4>
+                      <p>In January, the Gumala Trust (GIPL) congradulated Gumala Beneficiary Carol Lee Tucker when she took ownership of her home in Tom Price. Carol took advantage of the opportunity offered by GIPL last year for beneficiaries to purchace Gulama investment houses. The GIPL board is keen to see more Beneficiaries obtain their homes.
+                      </p>
+          
+          
+                  </div>
+                  
+                  <div class="col-md-6 meet-responsive">
+                      <img class="img-responsive" alt="carol" src="images/carol-img.jpg">
+                  </div>
+                  
+              </div>
+          </div>
+      </section>
+
+
+
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
