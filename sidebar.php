27d4a4d793781917c26e57a218f358dd4a1a1fb5
HayleyCollopy <61652264+HayleyCollopy@users.noreply.github.com>
2020-06-01 11:47:50 +0800

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,11 @@
+ <div class="col-md-3 sidebar">
+
+     <?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
+    <ul id="sidebar">
+        <?php dynamic_sidebar( 'sidebar1' ); ?>
+    </ul>
+<?php endif; ?>
+     
+     
+           
+   </div>    
