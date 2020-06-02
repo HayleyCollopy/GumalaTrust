20ac95b077cf72bc330c287259d4b997ed8b50d3
HayleyCollopy <61652264+HayleyCollopy@users.noreply.github.com>
2020-06-02 15:19:49 +0800

    Update board-of-directors.php

--- board-of-directors.php
+++ board-of-directors.php
@@ -8,12 +8,12 @@ Template Name: Board of Directors
 
 <section class="container-fluid text-center">
           <div class="container ">
-              <div class="bodtitle"><h2>BOARD OF DIRECTORS</h2></div>
-              <p class="welcome-text">The Gumala Trust Pty Ltd Board is comprised of six Directors. Three of these Directors are independent, with no material business or relation ties to the Beneficiaries of the Trust. The other three Directors are Traditional Owner Directors, one from each of the Bunjima, Yinhawangka and Nyiyaparli language groups.</p>
+              <div class="bodtitle"><h2><?php the_field('boardofdirectorstitle'); ?></h2></div>
+              <p class="welcome-text"><?php the_field('bodparagraphone'); ?></p>
 
-              <p class="welcome-text">GIPL Directors are appointed by the Gumala Aboriginal Corporation (GAC) Board. The positions are advertised and the appointment process is run and governed by the GAC Board.  The term of a Director is five years.</p>
+              <p class="welcome-text"><?php the_field('bodparagraphtwo'); ?></p>
 
-              <p class="welcome-text">To learn more about our Directors, please view their profiles below:</p>
+              <p class="welcome-text"><?php the_field('bodparagraphthree'); ?></p>
         </div>
                     
       </section>
@@ -25,25 +25,25 @@ Template Name: Board of Directors
           <div class="container"> 
               <div class="row">
                   <div class="col-md-6 meet-responsive text-center">
-                   <img class="img-responsive head-shot" alt="brendon-grylls" src="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/Brendon-Grylls.jpg">
-                      <h4 class="name">Brendon Grylls</h4>
-                      <p class="role">Role: Chairperson and Independent Director</p>
-                      <p class="committees">Committees: Audit and Risk (Proxy) and Investments</p>
+                   <img class="img-responsive head-shot" alt="brendon-grylls" src=<?php the_field('brendongryllsimg'); ?>>
+                      <h4 class="name"><?php the_field('brendongryllstitle'); ?></h4>
+                      <p class="role"><?php the_field('brendongryllsrole'); ?></p>
+                      <p class="committees"><?php the_field('brendongryllscommittees'); ?></p>
                   </div>
                   
                   <div class="col-md-6 meet-responsive">
                        
                       <p class="board-desc">
-                      Brendon Grylls brings a wealth of regional development and north west experience to the Gumala Foundation team. A background in small business, farming and manufacturing preceded a 16 year term in state parliament and he maintains an ongoing focus on indigenous policy and programs.
+                      <?php the_field('brendongryllsparagraphone'); ?>
                       </p>
                       <p class="board-desc">
-                      He is well known for his advocacy of regional issues, his commitment to expanding indigenous opportunities, the Royalties for Regions program and his leadership of the Nationals WA during the eight year Liberal/National Government.
+                      <?php the_field('brendongryllsparagraphtwo'); ?>
                       </p>
                       <p class="board-desc">
-                      Brendon has developed and delivered a host of new infrastructure and service programs across the North West, and has been a strong advocate for indigenous business development in the region. Projects such as Pilbara Cities, Ord Stage 2 and the Transitional Housing Program have included substantial policy reform initiatives to assist the indigenous community.
+                      <?php the_field('brendongryllsparagraphthree'); ?>
                       </p>
                       <p class="board-desc">
-                      As Chair of the Gumala Foundation he looks forward to working closely with the traditional owners of the Innawonga, Banyjima and Nyiyaparli language groups to honour the objectives of the Trust deed.
+                      <?php the_field('brendongryllsparagraphfour'); ?>
                       </p>
                       
                   </div>
@@ -59,25 +59,25 @@ Template Name: Board of Directors
           <div class="container"> 
               <div class="row">
                   <div class="col-md-6 meet-responsive text-center">
-                   <img class="img-responsive head-shot" alt="brendon-grylls" src="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/Brendon-Grylls.jpg">
-                      <h4 class="name">Brendon Grylls</h4>
-                      <p class="role">Role: Chairperson and Independent Director</p>
-                      <p class="committees">Committees: Audit and Risk (Proxy) and Investments</p>
+                   <img class="img-responsive head-shot" alt="irina-cattalini" src="<?php the_field('irinacattaliniimg'); ?>">
+                      <h4 class="name"><?php the_field('irinacattalinititle'); ?></h4>
+                      <p class="role"><?php the_field('irinacattalinirole'); ?></p>
+                      <p class="committees"><?php the_field('irinacattalinicommittees'); ?></p>
                   </div>
                   
                   <div class="col-md-6 meet-responsive">
                        
                       <p class="board-desc">
-                      Brendon Grylls brings a wealth of regional development and north west experience to the Gumala Foundation team. A background in small business, farming and manufacturing preceded a 16 year term in state parliament and he maintains an ongoing focus on indigenous policy and programs.
+                      <?php the_field('irinacattaliniparagraphone'); ?>
                       </p>
                       <p class="board-desc">
-                      He is well known for his advocacy of regional issues, his commitment to expanding indigenous opportunities, the Royalties for Regions program and his leadership of the Nationals WA during the eight year Liberal/National Government.
+                      <?php the_field('irinacattaliniparagraphtwo'); ?>
                       </p>
                       <p class="board-desc">
-                      Brendon has developed and delivered a host of new infrastructure and service programs across the North West, and has been a strong advocate for indigenous business development in the region. Projects such as Pilbara Cities, Ord Stage 2 and the Transitional Housing Program have included substantial policy reform initiatives to assist the indigenous community.
+                      <?php the_field('irinacattaliniparagraphthree'); ?>
                       </p>
                       <p class="board-desc">
-                      As Chair of the Gumala Foundation he looks forward to working closely with the traditional owners of the Innawonga, Banyjima and Nyiyaparli language groups to honour the objectives of the Trust deed.
+                      <?php the_field('irinacattaliniparagraphfour'); ?>
                       </p>
                       
                   </div>
@@ -93,26 +93,24 @@ Template Name: Board of Directors
           <div class="container"> 
               <div class="row">
                   <div class="col-md-6 meet-responsive text-center">
-                   <img class="img-responsive head-shot" alt="brendon-grylls" src="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/Brendon-Grylls.jpg">
-                      <h4 class="name">Brendon Grylls</h4>
-                      <p class="role">Role: Chairperson and Independent Director</p>
-                      <p class="committees">Committees: Audit and Risk (Proxy) and Investments</p>
+                   <img class="img-responsive head-shot" alt="brendon-grylls" src="<?php the_field('ianmcphersonimg'); ?>">
+                      <h4 class="name"><?php the_field('ianmcphersontitle'); ?></h4>
+                      <p class="role"><?php the_field('ianmcphersonrole'); ?></p>
+                      <p class="committees"><?php the_field('ianmcphersoncommittees'); ?></p>
                   </div>
                   
                   <div class="col-md-6 meet-responsive">
                        
                       <p class="board-desc">
-                      Brendon Grylls brings a wealth of regional development and north west experience to the Gumala Foundation team. A background in small business, farming and manufacturing preceded a 16 year term in state parliament and he maintains an ongoing focus on indigenous policy and programs.
+                      <?php the_field('ianmcphersonparagraphone'); ?>
                       </p>
                       <p class="board-desc">
-                      He is well known for his advocacy of regional issues, his commitment to expanding indigenous opportunities, the Royalties for Regions program and his leadership of the Nationals WA during the eight year Liberal/National Government.
+                          <?php the_field('ianmcphersonparagraphtwo'); ?>
                       </p>
                       <p class="board-desc">
-                      Brendon has developed and delivered a host of new infrastructure and service programs across the North West, and has been a strong advocate for indigenous business development in the region. Projects such as Pilbara Cities, Ord Stage 2 and the Transitional Housing Program have included substantial policy reform initiatives to assist the indigenous community.
+                      <?php the_field('ianmcphersonparagraphthree'); ?>
                       </p>
-                      <p class="board-desc">
-                      As Chair of the Gumala Foundation he looks forward to working closely with the traditional owners of the Innawonga, Banyjima and Nyiyaparli language groups to honour the objectives of the Trust deed.
-                      </p>
+
                       
                   </div>
                   
@@ -127,25 +125,28 @@ Template Name: Board of Directors
           <div class="container"> 
               <div class="row">
                   <div class="col-md-6 meet-responsive text-center">
-                   <img class="img-responsive head-shot" alt="brendon-grylls" src="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/Brendon-Grylls.jpg">
-                      <h4 class="name">Brendon Grylls</h4>
-                      <p class="role">Role: Chairperson and Independent Director</p>
-                      <p class="committees">Committees: Audit and Risk (Proxy) and Investments</p>
+                   <img class="img-responsive head-shot" alt="brendon-grylls" src="<?php the_field('bradleyhallimg'); ?>">
+                      <h4 class="name"><?php the_field('bradleyhalltitle'); ?></h4>
+                      <p class="role"><?php the_field('bradleyhallrole'); ?></p>
+                      <p class="committees"><?php the_field('bradleyhallcommittees'); ?></p>
                   </div>
                   
                   <div class="col-md-6 meet-responsive">
                        
                       <p class="board-desc">
-                      Brendon Grylls brings a wealth of regional development and north west experience to the Gumala Foundation team. A background in small business, farming and manufacturing preceded a 16 year term in state parliament and he maintains an ongoing focus on indigenous policy and programs.
+                      <?php the_field('bradleyhallparagraphone'); ?>
                       </p>
                       <p class="board-desc">
-                      He is well known for his advocacy of regional issues, his commitment to expanding indigenous opportunities, the Royalties for Regions program and his leadership of the Nationals WA during the eight year Liberal/National Government.
+                      <?php the_field('bradleyhallparagraphtwo'); ?>
                       </p>
                       <p class="board-desc">
-                      Brendon has developed and delivered a host of new infrastructure and service programs across the North West, and has been a strong advocate for indigenous business development in the region. Projects such as Pilbara Cities, Ord Stage 2 and the Transitional Housing Program have included substantial policy reform initiatives to assist the indigenous community.
+                      <?php the_field('bradleyhallparagraphthree'); ?>
                       </p>
                       <p class="board-desc">
-                      As Chair of the Gumala Foundation he looks forward to working closely with the traditional owners of the Innawonga, Banyjima and Nyiyaparli language groups to honour the objectives of the Trust deed.
+                      <?php the_field('bradleyhallparagraphfour'); ?>
+                      </p>
+                      <p class="board-desc">
+                      <?php the_field('bradleyhallparagraphfive'); ?>
                       </p>
                       
                   </div>
@@ -161,26 +162,21 @@ Template Name: Board of Directors
           <div class="container"> 
               <div class="row">
                   <div class="col-md-6 meet-responsive text-center">
-                   <img class="img-responsive head-shot" alt="brendon-grylls" src="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/Brendon-Grylls.jpg">
-                      <h4 class="name">Brendon Grylls</h4>
-                      <p class="role">Role: Chairperson and Independent Director</p>
-                      <p class="committees">Committees: Audit and Risk (Proxy) and Investments</p>
+                   <img class="img-responsive head-shot" alt="brendon-grylls" src="<?php the_field('helensmithimg'); ?>">
+                      <h4 class="name"><?php the_field('helensmithtitle'); ?></h4>
+                      <p class="role"><?php the_field('helensmithrole'); ?></p>
+                      <p class="committees"><?php the_field('helensmithcommittees'); ?></p>
                   </div>
                   
                   <div class="col-md-6 meet-responsive">
                        
                       <p class="board-desc">
-                      Brendon Grylls brings a wealth of regional development and north west experience to the Gumala Foundation team. A background in small business, farming and manufacturing preceded a 16 year term in state parliament and he maintains an ongoing focus on indigenous policy and programs.
+                      <?php the_field('helensmithparagraphone'); ?>
                       </p>
                       <p class="board-desc">
-                      He is well known for his advocacy of regional issues, his commitment to expanding indigenous opportunities, the Royalties for Regions program and his leadership of the Nationals WA during the eight year Liberal/National Government.
+                      <?php the_field('helensmithparagraphtwo'); ?>
                       </p>
-                      <p class="board-desc">
-                      Brendon has developed and delivered a host of new infrastructure and service programs across the North West, and has been a strong advocate for indigenous business development in the region. Projects such as Pilbara Cities, Ord Stage 2 and the Transitional Housing Program have included substantial policy reform initiatives to assist the indigenous community.
-                      </p>
-                      <p class="board-desc">
-                      As Chair of the Gumala Foundation he looks forward to working closely with the traditional owners of the Innawonga, Banyjima and Nyiyaparli language groups to honour the objectives of the Trust deed.
-                      </p>
+                      
                       
                   </div>
                   
@@ -195,26 +191,24 @@ Template Name: Board of Directors
           <div class="container"> 
               <div class="row">
                   <div class="col-md-6 meet-responsive text-center">
-                   <img class="img-responsive head-shot" alt="brendon-grylls" src="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/Brendon-Grylls.jpg">
-                      <h4 class="name">Brendon Grylls</h4>
-                      <p class="role">Role: Chairperson and Independent Director</p>
-                      <p class="committees">Committees: Audit and Risk (Proxy) and Investments</p>
+                   <img class="img-responsive head-shot" alt="brendon-grylls" src="<?php the_field('roytommyimg'); ?>">
+                      <h4 class="name"><?php the_field('roytommytitle'); ?></h4>
+                      <p class="role"><?php the_field('roytommyrole'); ?></p>
+                      <p class="committees"><?php the_field('roytommycommittees'); ?></p>
                   </div>
                   
                   <div class="col-md-6 meet-responsive">
                        
                       <p class="board-desc">
-                      Brendon Grylls brings a wealth of regional development and north west experience to the Gumala Foundation team. A background in small business, farming and manufacturing preceded a 16 year term in state parliament and he maintains an ongoing focus on indigenous policy and programs.
+                      <?php the_field('roytommyparagraphone'); ?>
                       </p>
                       <p class="board-desc">
-                      He is well known for his advocacy of regional issues, his commitment to expanding indigenous opportunities, the Royalties for Regions program and his leadership of the Nationals WA during the eight year Liberal/National Government.
+                      <?php the_field('roytommyparagraphtwo'); ?>
                       </p>
                       <p class="board-desc">
-                      Brendon has developed and delivered a host of new infrastructure and service programs across the North West, and has been a strong advocate for indigenous business development in the region. Projects such as Pilbara Cities, Ord Stage 2 and the Transitional Housing Program have included substantial policy reform initiatives to assist the indigenous community.
+                      <?php the_field('roytommyparagraphthree'); ?>
                       </p>
-                      <p class="board-desc">
-                      As Chair of the Gumala Foundation he looks forward to working closely with the traditional owners of the Innawonga, Banyjima and Nyiyaparli language groups to honour the objectives of the Trust deed.
-                      </p>
+
                       
                   </div>
                   
