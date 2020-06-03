c12492f4fad40f05c0ca3f23a7c012264ef67f81
HayleyCollopy <61652264+HayleyCollopy@users.noreply.github.com>
2020-06-04 03:22:51 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,11 @@
       <footer class="container-fluid">
           <div class="footer-content">
               <div class="text-center">
-<!--              SOCIAL MEDIA ICONS GO HERE-->
+                  
+                  
+            
+                  
+                  <!--              SOCIAL MEDIA ICONS GO HERE-->
               <a href="https://facebook.com/GumalaTrust"><img class="icons" src="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/facebook%20icon.png"></a>
               <a href="https://au.linkedin.com/company/gumala-trust"><img class="icons" src="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/linkedin%20icon.png"></a>
               <a href="mailto:gipl@gumalatrust.com"><img class="icons" src="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/mail_icon.png"></a>
@@ -20,14 +24,17 @@
               </div>
               
               </div>
-            <div class="row footerbox text-center">
+          
+          
+           <div class="row footerbox text-center">
+               
+                <div class="col-md-6img footerlogo">
+                  <a href="https://facebook.com/GumalaTrust"><img class="footerlogo" src="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/gumala-logo-icon.png"></a>
+                  </div>
+               
           <p class="copyrighttext">The Gumala Trust (GIPL) © 2020</p>
           </div>
-
           
       </footer>
 <!--      FOOTER ENDS-->
-      
-  </body>
-</html>
 <?php wp_footer(); ?>
