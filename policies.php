26fd7d5a6e8de4cd7d53d704d3b383acdb1618dd
georgiaearnshaw <61772678+georgiaearnshaw@users.noreply.github.com>
2020-06-04 22:50:37 +0800

    Update policies.php

--- policies.php
+++ policies.php
@@ -7,22 +7,17 @@ Template Name: BENEFICIARIES Page
 
 <?php include (TEMPLATEPATH . '/new_small_header.php');  /* Tells WordPress to include the smaller header header.php */ ?>
 
-
-      <!--      HEADER END-->
-      
-     <!--       WELCOME CONTAINER START-->
+<!--       WELCOME CONTAINER START-->
       <section class="container-fluid aboutbg text-center notfooter">
     <div class="container">
               
               <div class="beneficiary"><h2><?php the_field('titleforpolicies'); ?></h2>
+                  
+                     <div class="sub-title-beneficiary"><h2><?php the_field('subtitleforpolicies'); ?></h2>
                
-            <p class="sub-title-beneficiary"><?php the_field('subtitleforpolicies'); ?></p>
-                  
-                  <p class="beneficiary-text"><?php the_field('privacypolicytext'); ?></p>
-                  
-                      
+                   <p class="beneficiary-text"><?php the_field('privacypolicytext'); ?></p>                
  
-        </div>  </div>
+        </div>  </div> </div>
                     
       </section>
           
@@ -34,31 +29,21 @@ Template Name: BENEFICIARIES Page
  <section class="container-fluid meetcarolbg notfooter">
           <div class="container">
               <div class="row">
-                  <div class="trustlogo"><img src="images/gumala-logo-icon.png"></div>
+                  <div class="trustlogo"><img src="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/gumala-logo-icon.png"></div>
                   
                   <div class="col-md-6 beneficiary-container">
-                 <h4 class="application-process">Whistleblowing Policy</h4>
-                      <p class="members-info">The GGF is committed to the highest standards of good governance, legal, ethical and moral behaviour and in that regard recognises that its employees, contractors, suppliers and Beneficiaries may be the first to identify concerns of wrongdoing, either perceived or real.
-                          
-                          <br><br>
-
-The purpose of the Whistleblowing Policy is to provide a framework for a supportive work environment where legitimately held concerns of wrongdoing held by an Officer (Directors or Executives), Employee or Contractor providing services or goods to a GGF entity, can be raised without fear of reprisal.
-                          
-                          <br><br>
-                          
- Members or Beneficiaries wishing to raise complaints should refer to the GGF Complaints policy/process.
-                          
-                          <br><br>
-                          
-The policy will be reviewed every three years.
-                          
-                          
-                      </p>
+                      
+                        <p class="application-process"><?php the_field('whistleblowing'); ?></p>
+                      
+                        <p class="members-info"><?php the_field('whistleblowinginfo'); ?></p>
+                      
           
             </div>
                   
                   <div class="col-md-6 img-responsive-two">
-                      <img class="img-responsive" alt="landscape image" src="images/policies-page1.png">
+                      
+                      <img class="img-responsive" alt=“landscape-image” src="<?php the_field('whistleblowingimage'); ?>">
+                      
                   </div>
                   
               </div>
@@ -70,13 +55,16 @@ The policy will be reviewed every three years.
               <div class="row">
                   
                   <div class="col-md-6 meet-responsive">
-                  <img class="img-responsive" alt="landscape image" src="images/policies-image2.png">
+                      
+                      <img class="img-responsive" alt=“landscape-image” src="<?php the_field('groupmeetingsimage'); ?>">
+                      
                   </div>
                   
                   <div class="col-md-6 meet-responsive">
-                      <h4 class="application-reviewing">Group Meetings Policy</h4>
-                      <p class="beneficiary-text">The Group Meetings Policy has been developed to provide a guide to the Beneficiaries of the GGF on the procedures of meetings of the Foundation with specific consideration to the different types of meetings the Foundation may hold, the requirements of holding those meetings, conduct at meetings, proxy information, registrations information, allowances for attendees of the meetings and taxation.
-                      </p>
+                      
+                       <p class="application-reviewing"><?php the_field('groupmeetingstitle'); ?></p>
+                      
+                      <p class="beneficiary-text"><?php the_field('groupmeetingstext'); ?></p>
           
           
                   </div>      
