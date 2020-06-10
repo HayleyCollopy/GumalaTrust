d7b63eaa1a55b6766b6b404cd3fa1256701caf1a
HayleyCollopy <61652264+HayleyCollopy@users.noreply.github.com>
2020-06-10 14:53:25 +0800

    Create aboutus.php

---
+++ aboutus.php
@@ -0,0 +1,71 @@
+<?php
+/*
+Template Name: About Us Gumala
+*/
+?>
+
+<?php get_new_small_header(); ?>
+
+<!--       ABOUT US CONTAINER START-->
+      <section class="container-fluid aboutbg text-center notfooter">
+          <div class="container ">
+              <div class="welc"><h2>ABOUT US</h2></div>
+              <p class="welcome-text">The naming of the Gumala Foundation has an important meaning. In Bunjima language, Gumala means “all together”. This name is used because the three language groups – Bunjima, Yinhawangka and Nyiyaparli – combined to negotiate the Yandi Land Use Agreement (YLUA) and the General Gumala Foundation Trust Deed (Trust Deed).</p>
+
+                  <p class="welcome-text">This historic YLUA signed in 1997 by Hamersley Iron Pty Ltd (now owned by Rio Tinto) was the first of its kind in Western Australia, enabling mining activity on Traditional Land in exchange for compensation paid to the Traditional Owners. The Trust Deed was signed in the same year.</p>
+
+                      <p class="welcome-text">The Trust Deed is a comprehensive, 34-page legal document between Gumala Aboriginal Corporation (The Manager), Gumala Investments Pty Ltd (The Trustee), and the Traditional Owners of the three language groups which make up Gumala.</p>
+        </div>
+                    
+      </section>
+      <!--       ABOUT US CONTAINER END-->
+      
+      <section class="container-fluid meetcarolbg notfooter">
+          <div class="container">
+              <div class="row">
+                  
+                  <div class="col-md-6 meet-responsive">
+                      <h4 class="carol">GUMALA TRUST</h4>
+                      <p class="carolp">GIPL’s roles and responsibilities are outlined in the General Gumala Foundation Trust Deed. The Trust Deed is a legally binding document that gives GIPL important powers to ensure funds are spent correctly, and in strict accordance with the terms of the Trust Deed.</p>
+
+                      <p class="carolp">The Trust Deed specifies the Income Utilisation Categories (IUCs) against which income of the Trust, including the Yandi Land Use Agreement, can be allocated towards for improving the lives and opportunities of Beneficiaries. </p>
+                      <p class="seemore">See more...</p>
+          
+          
+                  </div>
+                  
+                  <div class="col-md-6img meet-responsive">
+                  </div>
+                  
+              </div>
+          </div>
+      </section>
+      
+      <section class="container-fluid meetdavidbg notfooter">
+          <div class="container">
+              <div class="row">
+                  
+                  <div class="col-md-6 meet-responsive">    
+                  <img class="img-responsive" alt="david" src="http://206.189.45.97/~mesh4/wp-content/themes/gumala_trust/images/directors-and-staff.jpg">
+                      
+                  </div>
+                  
+                  <div class="col-md-6 meet-responsive">
+                      <h4 class="david">BOARD OF DIRECTORS</h4>
+                      <p class="davidp">The Gumala Trust Pty Ltd Board is comprised of six Directors. Three of these Directors are independent, with no material business or relation ties to the Beneficiaries of the Trust. The other three Directors are Traditional Owner Directors, one from each of the Bunjima, Yinhawangka and Nyiyaparli language groups.</p>
+
+                          <p class="davidp">GIPL Directors are appointed by the Gumala Aboriginal Corporation (GAC) Board. The positions are advertised and the appointment process is run and governed by the GAC Board.  The term of a Director is five years.</p>
+                      <p class="seemore">See more...</p>
+                      
+                        
+          
+          
+                  </div>
+                  
+                  <div class="text-center">
+                  <button type="button" class="btn btn-primary text-center">SEE BOARD OF DIRECTORS</button>
+              </div>
+              </div>
+          </div>
+      </section> 
+<?php get_footer(); ?>
